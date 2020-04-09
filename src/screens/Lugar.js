23c250916d808e81555dc246e47b766b7e0b5fb1
Juan David <gottygarcia@gmail.com>
2020-04-08 19:38:55 -0500

    avances en pantalla de lugares

--- src/screens/Lugar.js
+++ src/screens/Lugar.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import styles from '../styles'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const Lugar = () => {
 	return (
-		<View>
-			<Text>Lugar</Text>
+		<View style={styles.lugar_container}>
+			<View style={styles.header_lugar}>
+				<Image
+					style={styles.img_lugar}
+					source={require('../img/lugares/1.jpg')}
+				/>
+				<View style={styles.title_header}>
+					<Image
+						style={styles.icon_lugar}
+						source={require('../img/location.png')}
+					/>
+					<Text style={styles.nombre_lugar}>Teatro Jorge Isaacs</Text>
+				</View>
+				<LinearGradient
+					colors={['transparent', 'white']}
+					style={styles.gradient_semana}
+				></LinearGradient>
+			</View>
+			<View style={styles.contenido_lugar}>
+				<View style={styles.texto_lugar}>
+					<Text style={{ fontWeight: 'bold' }}>Año: </Text>
+					<Text>1931</Text>
+				</View>
+				<View style={styles.texto_lugar}>
+					<Text style={{ fontWeight: 'bold' }}>Topología: </Text>
+					<Text>Edificación para espectáculos</Text>
+				</View>
+				<View style={styles.texto_lugar}>
+					<Text style={{ fontWeight: 'bold' }}>Promotores: </Text>
+					<Text>
+						Ingenieros Gaetano Lignarolo y Guido Micucci. Decorado por Mauricio
+						Ramelli. Intervenido por Manuel Lago (DAPM, 2014).
+					</Text>
+				</View>
+			</View>
 		</View>
 	)
 }
--- src/styles.js
+++ src/styles.js
@@ -3,18 +3,76 @@ import { StyleSheet, Dimensions } from 'react-native'
 const styles = StyleSheet.create({
 	containerNavbar: {
 		width: Dimensions.get('window').width,
-		height: 60,
+		height: '8%',
 		backgroundColor: '#5AAFFE',
 		position: 'absolute',
 		bottom: 0,
 		display: 'flex',
 		alignItems: 'center',
-		justifyContent: 'space-around',
+		justifyContent: 'space-between',
 		flexDirection: 'row',
+		zIndex: 1,
 	},
 	text_navbar: {
 		color: 'white',
 		fontSize: 10,
+	},
+	navbar_item: {
+		alignItems: 'center',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		height: '100%',
+		width: Dimensions.get('window').width / 4,
+	},
+	icon_navbar: {
+		width: 26,
+		height: 26,
+	},
+	opacity: {
+		opacity: 0.6,
+	},
+	img_lugar: {
+		width: '100%',
+		height: '100%',
+		opacity: 0.7,
+	},
+	lugar_container: {
+		height: '92%',
+		zIndex: 0,
+	},
+	header_lugar: {
+		height: '45%',
+		backgroundColor: 'black',
+	},
+	title_header: {
+		position: 'absolute',
+		bottom: 65,
+		left: 35,
+	},
+	nombre_lugar: {
+		color: 'white',
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	icon_lugar: {
+		width: 26,
+		height: 26,
+		marginBottom: 10,
+	},
+	gradient_semana: {
+		width: '100%',
+		height: '20%',
+		position: 'absolute',
+		bottom: 0,
+	},
+	contenido_lugar: {
+		paddingVertical: 10,
+		paddingHorizontal: 35,
+	},
+	texto_lugar: {
+		display: 'flex',
+		flexDirection: 'row',
 	},
 })
 
