38a0ed1b415065036dae336bdba8b163724929a2
Juan David <gottygarcia@gmail.com>
2020-06-10 20:48:21 -0500

    Rutas lugares más cercanos

--- src/screens/Mapa.js
+++ src/screens/Mapa.js
@@ -10,42 +10,84 @@ import polyline from '@mapbox/polyline'
 
 const Mapa = () => {
 	const history = useHistory()
+	const RutasColores = ['#5AAFFE', '#8AFF57', '#FFAB57', '#EB57FF', '#FF5757']
 
 	const [region, setRegion] = useState(null)
-	const [MasCercano, setMasCercano] = useState(null)
+	const [MasCercanoArray, setMasCercanoArray] = useState(null)
 	const [Ruta, setRuta] = useState(null)
 
 	useEffect(() => {
-		navigator.geolocation.getCurrentPosition((position) => {
+		navigator.geolocation.getCurrentPosition(async (position) => {
 			const lat = position.coords.latitude
 			const lon = position.coords.longitude
 
 			const current = { lat, lon }
+
+			const accuracy = position.coords.accuracy
+			setRegion(current)
+			const LugaresCercanos = CalcularMasCercanos(current)
+			const RutaDirecciones = []
+			RutaDirecciones.push(await getDirections(current, LugaresCercanos[1]))
+			RutaDirecciones.push(
+				await getDirections(LugaresCercanos[1], LugaresCercanos[2])
+			)
+			RutaDirecciones.push(
+				await getDirections(LugaresCercanos[2], LugaresCercanos[3])
+			)
+			RutaDirecciones.push(
+				await getDirections(LugaresCercanos[3], LugaresCercanos[4])
+			)
+			RutaDirecciones.push(
+				await getDirections(LugaresCercanos[4], LugaresCercanos[5])
+			)
+			setRuta(RutaDirecciones)
+			//calDeta(lat, lon, accuracy)
+		})
+	}, [])
+
+	const CalcularMasCercanos = (lugarInicial) => {
+		const LugaresCernanos = [lugarInicial]
+
+		do {
 			const closest = lugares
 				.map((lugar) => {
 					const coord = {
 						lat: lugar.gps[0],
-						lng: lugar.gps[1],
+						lon: lugar.gps[1],
 					}
-					return { coord, dist: geolib.getDistance(current, coord) }
+
+					if (
+						coord.lat !== LugaresCernanos[LugaresCernanos.length - 1].lat &&
+						coord.lon !== LugaresCernanos[LugaresCernanos.length - 1].lon
+					) {
+						return {
+							coord,
+							dist: geolib.getDistance(
+								LugaresCernanos[LugaresCernanos.length - 1],
+								coord
+							),
+						}
+					}
 				})
 				.sort((a, b) => a.dist - b.dist)[0]
-			setMasCercano(closest.coord)
 
-			const accuracy = position.coords.accuracy
-			calDeta(lat, lon, accuracy)
-		})
-		getDirections(region, MasCercano)
-	}, [])
+			const YaEsta = LugaresCernanos.findIndex(
+				(element) => element === LugaresCernanos[2]
+			)
 
-	useEffect(() => {
-		getDirections(region, MasCercano)
-	}, [region])
+			LugaresCernanos.push(closest.coord)
+		} while (LugaresCernanos.length !== 6)
+
+		console.log('---------')
+		console.log(LugaresCernanos)
+
+		return LugaresCernanos
+	}
 
 	const getDirections = async (startLoc, desLoc) => {
 		try {
 			const resp = await fetch(
-				`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc.latitude},${startLoc.longitude}&destination=${desLoc.lat},${desLoc.lng}&key=AIzaSyAbOQiex_C_CJzUPH9O4xIPfkFrNOqGDJQ`
+				`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc.lat},${startLoc.lon}&destination=${desLoc.lat},${desLoc.lon}&key=AIzaSyAbOQiex_C_CJzUPH9O4xIPfkFrNOqGDJQ`
 			)
 			const respJson = await resp.json()
 			const response = respJson.routes[0]
@@ -61,30 +103,30 @@ const Mapa = () => {
 					longitude: point[1],
 				}
 			})
-			setRuta(coords)
+			return coords
 		} catch (error) {
 			console.log('Error: ', error)
 		}
 	}
 
-	const calDeta = (lat, lon, accuracy) => {
-		const oneDegreeOfLongitudInMeters = 111.32
-		const circunference = 40075 / 360
-		const latDelta = accuracy * (1 / (Math.cos(lat) * circunference))
-		const lonDelta = accuracy / oneDegreeOfLongitudInMeters
+	// const calDeta = (lat, lon, accuracy) => {
+	// 	const oneDegreeOfLongitudInMeters = 111.32
+	// 	const circunference = 40075 / 360
+	// 	const latDelta = accuracy * (1 / (Math.cos(lat) * circunference))
+	// 	const lonDelta = accuracy / oneDegreeOfLongitudInMeters
 
-		setRegion({
-			latitude: lat,
-			longitude: lon,
-			latitudeDelta: latDelta,
-			longitudeDelta: lonDelta,
-		})
-	}
+	// 	setRegion({
+	// 		lat: lat,
+	// 		lon: lon,
+	// 		latitudeDelta: latDelta,
+	// 		longitudeDelta: lonDelta,
+	// 	})
+	// }
 
 	const MiUbicacion = () => {
 		return {
-			latitude: region.latitude,
-			longitude: region.longitude,
+			latitude: region.lat,
+			longitude: region.lon,
 		}
 	}
 
@@ -106,9 +148,18 @@ const Mapa = () => {
 						image={require('../img/me2.png')}
 					/>
 				) : null}
-				{Ruta ? (
-					<Polyline coordinates={Ruta} strokeColor="#5AAFFE" strokeWidth={5} />
-				) : null}
+				{Ruta
+					? Ruta.map((linea, index) => {
+							return (
+								<Polyline
+									key={index}
+									coordinates={linea}
+									strokeColor={RutasColores[index]}
+									strokeWidth={5}
+								/>
+							)
+					  })
+					: null}
 				{lugares.map((lugar) => {
 					return (
 						<Marker
